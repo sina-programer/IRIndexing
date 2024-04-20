f50aeea7e6161e3b44332839f868d282c302a288
Sina.F <sina.programerr@gmail.com>
2024-04-20 16:57:18 +0330

    Update compress.py

--- compress.py
+++ compress.py
@@ -1,16 +1,11 @@
-def binary(number, keep_prefix=False):
+def binary(number, prefix=False):
     binarized = bin(number)
-    if keep_prefix:
+    if prefix:
         return binarized
     return binarized[2:]
 
-
 def unary(number):
-    clause = ''
-    if number:
-        clause = number*'1'
-    return clause + '0'
-
+    return '1'*number + '0'
 
 def gamma(number):
     if number == 0:
@@ -19,13 +14,11 @@ def gamma(number):
     offset = number_b[1:]
     return unary(len(offset)) + offset
 
-
 def variable_byte(number, n=8):
     np = n-1
     result = ''
     number_b = binary(number)
     n_packs = (len(number_b) // n) + 1
-
     for i in range(n_packs):
         pack = format(number_b[i*np : (i+1)*np], f'0>{np}')
         is_last_iter = i == n_packs-1
@@ -33,27 +26,23 @@ def variable_byte(number, n=8):
             result = '1' + pack + result
         else:
             result = '0' + pack + result
-
     return result
 
-
-def get_size(bin_clause):
-    return len(bin_clause)
+def get_size(x):
+    if isinstance(x, int):
+        return x.bit_length()
+    elif isinstance(x, (str, bytes)):
+        return len(x)
 
 
 if __name__ == "__main__":
-    _numbers = [0, 54, 692, 833, 1502, 1802]
-    print('Numbers:', _numbers)
-
-    _binarized = list(map(binary, _numbers))
-    _variable_code = list(map(variable_byte, _numbers))
-    _gamma_code = list(map(gamma, _numbers))
-    print('\nBinary Form')
-    print('Primary Data: ', _binarized)
-    print('Variable Byte: ', _variable_code)
-    print('Gamma Code: ', _gamma_code)
-
-    print('\nSize (bits)')
-    print('Primary Data: ', sum(map(get_size, _binarized)))
-    print('Variable Byte: ', sum(map(get_size, _variable_code)))
-    print('Gamma Code: ', sum(map(get_size, _gamma_code)))
+    test_cases = [
+        'binary(5)',
+        'unary(5)',
+        'gamma(5)',
+        'variable_byte(5)',
+        'get_size(5)'
+    ]
+    for case in test_cases:
+        print(f"{case} = {eval(case)}")
+    print()
